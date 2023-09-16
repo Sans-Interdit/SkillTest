95f5d4960a7ca2b0e69d3fffe1d0fda406d2f28d
Yanis Bouchilloux <bouchilloux.yanis@gmail.com>
2023-09-16 17:00:45 +0200

    add project delete

--- angular/src/app/login-page/login-page.component.ts
+++ angular/src/app/login-page/login-page.component.ts
@@ -9,20 +9,31 @@ import { Router } from '@angular/router';
   styleUrls: ['./login-page.component.scss'],
 })
 export class LoginPageComponent {
+
+  message : string = "Identifiant :";
   loggin : string = "";
   constructor(private globalVar : GlobalVar, private readonly apiService : ApiService, private router : Router){}
   beLoggedIn(){
     if (this.loggin != ""){
       this.apiService.getAccounts(this.loggin).subscribe((logged : boolean) => {
         if (logged){
-          console.log()
           this.globalVar.account = this.loggin;
           this.router.navigate(['projects']);
         }
+        else{
+          this.uncorrectID()
+        }
       })
+    }
+    else{
+      this.uncorrectID()
     }
   }
   changeLoggin(event : any){
     this.loggin = event.target.value;
+  }
+  uncorrectID(){
+    this.message = "Saisie Invalide";
+    this.loggin = "";
   }
 }
